40a8e807646f0fdb236f5b5219b7a91346696b81
angelajuang <juang.angela@gmail.com>
2011-09-24 11:38:36 -0700

    disabled create button on click

--- static/js/create_task.js
+++ static/js/create_task.js
@@ -1,11 +1,13 @@
 $(function () {
   $('#create_form').submit(function (event) {
     event.preventDefault();
+	$('#create_button').attr('disabled', 'disabled');
 	$.ajax({
 	  type: 'POST',
 	  url: '/task/make/',
 	  data: {'content': $('#task_text').val(), 'title': $('#task_title').val()},
 	  success: function(response) {
+	    $('#create_button').removeAttr('disabled');
         $('#main_content').load('ajax/home/');
 	    $('#top_create').attr('class', '');
 	    $('#top_mytasks').attr('class', 'active');	  

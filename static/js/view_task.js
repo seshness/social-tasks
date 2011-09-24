86ba16f5f26a38e77d9738b939f61d925383eae5
angelajuang <juang.angela@gmail.com>
2011-09-24 11:54:46 -0700

    MY BAD

--- static/js/view_task.js
+++ static/js/view_task.js
@@ -2,11 +2,12 @@ $(function () {
   $('#create_comment').submit(function (event) {
     event.preventDefault();
 	$('#post_button').attr('disabled', 'disabled');
+	var comment = $('#comment_text').val();
 	$('#comment_text').val('');
 	$.ajax({
 	  type: 'POST',
 	  url: '/task/comment/',
-	  data: {'content': $('#comment_text').val(), 'task_id': $('#comment_text').attr('task_id')},
+	  data: {'content': comment, 'task_id': $('#comment_text').attr('task_id')},
 	  success: function(response) {
         $('#comments').append(response);
 		$('#post_button').removeAttr('disabled');

33b8d4fa299e8a967b05eb074fa5076450878f68
angelajuang <juang.angela@gmail.com>
2011-09-24 11:52:04 -0700

    made commenting more snazzy

--- static/js/view_task.js
+++ static/js/view_task.js
@@ -1,12 +1,15 @@
 $(function () {
   $('#create_comment').submit(function (event) {
     event.preventDefault();
+	$('#post_button').attr('disabled', 'disabled');
+	$('#comment_text').val('');
 	$.ajax({
 	  type: 'POST',
 	  url: '/task/comment/',
 	  data: {'content': $('#comment_text').val(), 'task_id': $('#comment_text').attr('task_id')},
 	  success: function(response) {
         $('#comments').append(response);
+		$('#post_button').removeAttr('disabled');
 	  }
 	});
   });

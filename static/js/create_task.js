636098ef1f270c50629d246244a5a6c310dc9891
angelajuang <juang.angela@gmail.com>
2011-09-24 04:18:14 -0700

    added stuff to post task from create task view

---
+++ static/js/create_task.js
@@ -0,0 +1,15 @@
+$(function () {
+  $('#submit_task').click(function (event) {
+    event.preventDefault();
+	.ajax({
+	  type: 'POST',
+	  url: '/task/make/',
+	  data: $('#task_text').val();
+	  success: function(response) {
+        $('#main_content').load('ajax/home/');
+	    $('#top_create').attr('class', '');
+	    $('#top_mytasks').attr('class', 'active');	  
+	  }
+	});
+  });
+});

30437eba610541494db8df08f0b67152355c31ca
angelajuang <juang.angela@gmail.com>
2011-09-24 02:30:55 -0700

    added some event binding to view tasks

--- static/js/home.js
+++ static/js/home.js
@@ -3,4 +3,9 @@ $(function () {
      event.preventDefault();
      $('#main_content').load('task/create');
   });
+  
+  $('td .task_entry').click(function (event) {
+    event.preventDefault();
+	$('main_content').load('task/'+$('this').id);
+  });
 });

9781c69730422beb79e4232b34b3262548445dd0
Seshadri Mahalingam <seshadri@berkeley.edu>
2011-09-24 01:49:00 -0700

    /task/create

--- static/js/home.js
+++ static/js/home.js
@@ -1,11 +1,9 @@
 (function ( $ ) {
   function prepareCreateTask() {
     $('#create_task').click(function (event) {
-	  event.preventDefault();
-	  $('#main_content').load('create_task.html');
-	});
+        event.preventDefault();
+        $('#main_content').load('/task/create');
+      });
   }
-  
   prepareCreateTask();
-
 })( window.jQuery );

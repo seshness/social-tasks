dc42e27cd9affc0ebc88b6a1a5b4f22228d4e21b
angelajuang <juang.angela@gmail.com>
2011-09-24 04:32:49 -0700

    debug statements for js

--- static/js/create_task.js
+++ static/js/create_task.js
@@ -1,11 +1,13 @@
 $(function () {
   $('#submit_task').click(function (event) {
     event.preventDefault();
+	alert('sending ajax post');
 	.ajax({
 	  type: 'POST',
 	  url: '/task/make/',
 	  data: {'content': $('#task_text').val()},
 	  success: function(response) {
+	    alert('successful post');
         $('#main_content').load('ajax/home/');
 	    $('#top_create').attr('class', '');
 	    $('#top_mytasks').attr('class', 'active');	  

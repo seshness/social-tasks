09cb62fbb474a816df3d64fbc29a63e5704c5da2
angelajuang <juang.angela@gmail.com>
2011-09-24 10:23:03 -0700

    fixing comments

--- static/js/create_task.js
+++ static/js/create_task.js
@@ -1,13 +1,11 @@
 $(function () {
   $('#create_form').submit(function (event) {
     event.preventDefault();
-	alert('sending ajax post');
 	$.ajax({
 	  type: 'POST',
 	  url: '/task/make/',
 	  data: {'content': $('#task_text').val(), 'title': $('#task_title').val()},
 	  success: function(response) {
-	    alert('successful post');
         $('#main_content').load('ajax/home/');
 	    $('#top_create').attr('class', '');
 	    $('#top_mytasks').attr('class', 'active');	  
--- static/js/view_task.js
+++ static/js/view_task.js
@@ -3,8 +3,8 @@ $(function () {
     event.preventDefault();
 	$.ajax({
 	  type: 'POST',
-	  url: '/task/make/',
-	  data: {'content': $('#task_text').val()},
+	  url: '/task/comment/',
+	  data: {'content': $('#comment_text').val(), 'task_id': $('#comment_text').attr('task_id')},
 	  success: function(response) {
         alert('created comment');
 	  }
